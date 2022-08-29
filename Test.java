c77afe4aacdf53b4384acc516c086c2e312571e2
yegireddihemanth <84504741+yegireddihemanth@users.noreply.github.com>
2022-08-29 13:04:08 +0530

    java file added for cicd testing

---
+++ Test.java
@@ -0,0 +1,22 @@
+// rules
+// can have comments anyway
+// a package decletation - 0 - 1 decleration only - optional
+		// if present should be on the first line of  .java file after the 
+		// comments if any
+
+// package decleration is followed by comments and/or  import statements or 
+	//class/interface/enum declaration
+
+// can have 0 or n import statements - optional
+
+//import can be followed by comments and/or class/interface/enum/declarations
+
+//out of all the classes /interfaces/enums - only one can be made public - 
+// when a public class/interfaces/enum/ is made public then .java file name should also have the same name should aslo have the same name
+
+public class Test{
+public static void main(String args[]){
+System.out.println("hello hemanth");
+}
+
+}
